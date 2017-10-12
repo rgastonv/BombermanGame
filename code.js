6903c39357e7a4c2e8fd07faafd10a4052e26b2a
ElenaChueca <32261249+ElenaChueca@users.noreply.github.com>
2017-10-12 14:24:20 +0200

    Add files via upload
    Modificada caja de colisiones del personaje
    Bucle for anidado en el de creación de bloques de metal para probar el funcionamiento de la caja de colisiones reducida.

--- code.js
+++ code.js
@@ -3,8 +3,8 @@ var game = new Phaser.Game(568,704 , Phaser.AUTO, '', { preload: preload, create
 
 function preload() {
 
-    game.load.spritesheet('dude', 'assets/dude.png', 32, 48); //Cada 32 píxeles recorta un nuevo sprite horizontalmente
 
+    game.load.spritesheet('prota', 'Sprites/Sprites_prota_1.png', 38, 64);
     game.load.image('fondo', 'Sprites/fondo.png');
     game.load.image('piedra', 'Sprites/Sprites_bloque_piedra.png');
     game.load.image('ladrillo', 'Sprites/Sprites_bloque_ladrillo.png');
@@ -15,9 +15,11 @@ function preload() {
 
 var player;
 var cursors;
-var stars;
+
 
 var ladrillos; //Para el grupo
+var piedras;
+var piedrasBordes;
 
 
 var score = 0;
@@ -25,61 +27,60 @@ var scoreText;
 
 function create() {
 
-    //  We're going to be using physics, so enable the Arcade Physics system
-    game.physics.startSystem(Phaser.Physics.ARCADE);
-
+    
     //  A simple background for our game
     game.add.sprite(0,0,'fondo');
 
     //  The platforms group contains the ground and the 2 ledges we can jump on
     
     ladrillos = game.add.group();
+    piedras = game.add.group();
+    piedrasBordes = game.add.group();
     
 
     //  We will enable physics for any object that is created in this group
     ladrillos.enableBody = true;
+    piedras.enableBody = true;
+    piedrasBordes.enableBody = true;
 
-    // Here we create the ground.
-    var bloqueLadrillo = ladrillos.create(56,game.world.height - 128,'ladrillo2');
     
     //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
     //ground.scale.setTo(2, 2);
 
-    //  This stops it from falling away when you jump on it
-    bloqueLadrillo.body.immovable = true;
     
     // The player and its settings
-    player = game.add.sprite(32, game.world.height - 150, 'dude');
-
+    player = game.add.sprite(32, game.world.height - 150, 'prota');
+  
     //  We need to enable physics on the player
     game.physics.arcade.enable(player);
 
-    //  Player physics properties. Give the little guy a slight bounce.
 
     player.body.collideWorldBounds = true;
+    player.body.setSize(16, 20, 8, 38); //Caja de colisiones del personaje. Se puede modificar más detalladamente. No poner más abajo que peta. 
 
     //  Our two animations, walking left and right.
-    player.animations.add('left', [0, 1, 2, 3], 10, true); //Se crea una animación "left" con los primeros cuatro sprites de "dude", a 10 fps y con loop (true)
-    player.animations.add('right', [5, 6, 7, 8], 10, true);
+    player.animations.add('left', [0, 1, 2, 3,4], 10, true); //Se crea una animación "left" con los primeros cuatro sprites de "dude", a 10 fps y con loop (true)
+    player.animations.add('right',  [0, 1, 2, 3,4],10, true);
 
-    //  Finally some stars to collect
-    stars = game.add.group();
-
-    //  We will enable physics for any star that is created in this group
-    stars.enableBody = true;
-
-    //  Here we'll create 12 of them evenly spaced apart
-    for (var i = 0; i < 12; i++)
+   
+    for(var i = 0; i < 12; i++)
     {
-        //  Create a star inside of the 'stars' group
-        var star = stars.create(i * 70, 0, 'star');
+        //  Create a star inside of the 'LADRILLO' group
+        var bloqueLadrillo = ladrillos.create(i * 30, 0, 'ladrillo2');
+        bloqueLadrillo.body.immovable = true;
 
-        //  Let gravity do its thing
-        star.body.gravity.y = 300;
-
-        //  This just gives each star a slightly random bounce value
-        star.body.bounce.y = 0.7 + Math.random() * 0.2;
     }
+    for(var i = 0; i < 12; i++)
+    {
+        for (var j=0; j<5; j++){
+        //  Creamos filas y columnas de piedras para comprobar colisiones. 
+        var bloquePiedra = piedras.create(i * 70, j*90, 'piedra2');
+        bloquePiedra.body.immovable = true;
+        }
+
+
+    }
+
 
     //  The score
     scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
@@ -91,14 +92,11 @@ function create() {
 
 function update() {
 
-    //  Collide the player and the stars with the platforms
-    //game.physics.arcade.collide(player, platforms);
-    //game.physics.arcade.collide(stars, platforms);
+
 
     game.physics.arcade.collide(player, ladrillos);
+    game.physics.arcade.collide(player, piedras);
 
-    //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
-    game.physics.arcade.overlap(player, stars, collectStar, null, this);
 
     //  Reset the players velocity (movement)
     player.body.velocity.x = 0;
@@ -133,21 +131,6 @@ function update() {
         player.frame = 4;
     }
     
-    //  Allow the player to jump if they are touching the ground.
-    if (cursors.up.isDown && player.body.touching.down)
-    {
-        player.body.velocity.y = -350;
-    }
 
 }
 
-function collectStar (player, star) {
-    
-    // Removes the star from the screen
-    star.kill();
-
-    //  Add and update the score
-    score += 10;
-    scoreText.text = 'Score: ' + score;
-
-}
